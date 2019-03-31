907ba2ab3758fbdd1b68a9d80696c5b2b12744a0
Andy Shih <AndyShih51@gmail.com>
2019-03-31 08:38:39 -0700

    Add wizard-adventure

---
+++ main.lisp
@@ -0,0 +1,10 @@
+;;;; MAIN for WIZARD ADVENTURE
+
+(load "wizard-adventure.lisp")
+(load "game-repl.lisp")
+
+;;; Compile with SBCL
+;;; sbcl --script main.lisp
+(sb-ext:save-lisp-and-die "wizard-adventure"
+    :toplevel #'game-repl
+    :executable t)
